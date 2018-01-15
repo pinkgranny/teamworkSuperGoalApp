3d0a3bcf87a648c3486ad4eecf53979da692ea09
kmkkkuk <kmkkuk@gmail.com>
2018-01-15 13:27:18 +0100

    BrowserRouter basename for public url added

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -108,7 +108,7 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <div className="App">
           <Header />
 
